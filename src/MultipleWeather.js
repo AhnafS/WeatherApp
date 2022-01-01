539ed30e113e7f28d3b31eb40f09c3d0be4c0033
Ahnaf Shamim <ahnafs@Ahnafs-MacBook-Air.local>
2022-01-01 15:13:28 -0500

    Finished fundementals of app

--- src/MultipleWeather.js
+++ src/MultipleWeather.js
@@ -1,8 +1,12 @@
 import { React, useState, useEffect } from "react";
 import SingleWeather from "./SingleWeather";
+import data from "./data";
+import Loading from "./Loading";
 
 const MultipleWeather = ({ coord, city }) => {
-  let [weathers, setWeathers] = useState({});
+  let [weathers, setWeathers] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   const getWeathers = async () => {
     const { lon, lat } = coord;
@@ -10,21 +14,33 @@ const MultipleWeather = ({ coord, city }) => {
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${process.env.REACT_APP_KEY}&units=imperial`;
     try {
       const response = await fetch(url);
+      console.log(`Response of ONE CALL API: ${response.status}`);
       const newWeathers = await response.json();
       setWeathers(newWeathers.daily);
-      console.log(newWeathers);
+      setLoading(false);
+      console.log(weathers);
     } catch (err) {
       console.log(err);
     }
   };
 
+  const prevButton = () => setIndex(index - 1);
+  const nextButton = () => setIndex(index + 1);
+
   useEffect(() => {
     getWeathers();
+    console.log("MultipleWeather useEffect");
   }, [coord]);
 
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <section className="h-1/2 w-2/4 bg-gray-50 mt-5 lg:mt-0">
-      <SingleWeather {...weathers[3]} city={city} />
+    <section className="h-1/2 w-3/4 bg-gray-50 mt-5 lg:mt-0 flex overflow-x-scroll">
+      {weathers.map((weather, i) => {
+        return <SingleWeather {...weathers[i]} city={city} key={i} />;
+      })}
     </section>
   );
 };
--- src/SingleWeather.js
+++ src/SingleWeather.js
@@ -7,38 +7,30 @@ const SingleWeather = ({ dt, temp, pressure, weather, city }) => {
   weather = weather[0];
   const { main, description, icon: preIcon } = weather;
 
-  const unixDateFormater = () => {
-    const milliseconds = dt * 1000;
-    const dateObject = new Date(milliseconds);
-    const humanDateFormat = dateObject
-      .toString()
-      .split(" ")
-      .slice(0, 4)
-      .join(" ");
-    setDate(humanDateFormat);
-    console.log(`The Date from SingleWeather ${date}`);
-  };
-
   useEffect(() => {
-    unixDateFormater();
     getIcon(preIcon).then((newIcon) => setIcon(newIcon));
+    console.log("SingleWeather useEffect");
   }, []);
 
+  console.log("Im here");
   return (
-    <section className="h-full bg-gray-50 rounded p-6 bg-white flex items-center justify-around flex-col text-xl shadow-md p-5 text-center capitalize pt-px">
+    <section className="h-full rounded p-6 bg-white flex items-center justify-around flex-col text-xl shadow-lg p-5 text-center capitalize pt-px lg:w-2/4 hover:border-2">
       <div className="border-b-4 border-indigo-500">
         <h1>{city}</h1>
-        <h2>{date}</h2>
+        <h2>
+          {new Date(dt * 1000).toString().split(" ").slice(0, 4).join(" ")}
+        </h2>
       </div>
       <div className="flex justify-center flex-col items-center pt-px">
         <h4>{main}</h4>
         <img src={icon} alt={city} />
         <h4>{description}</h4>
       </div>
-      <div className="flex  justify-between border-t-4 border-indigo-500 py-1">
+      <div className="flex  justify-between border-t-4 border-indigo-500 py-1 items-center">
         <h3 className="mr-2">Max: {Math.round(temp.max)}</h3>
         <h3> Min: {Math.round(temp.min)}</h3>
-        <h4>Pressure: {pressure}</h4>
+        <br />
+        <h4>Pressure: {pressure} </h4>
       </div>
     </section>
   );
