836f080eb788a717f9092c55382923ca80245463
Ahnaf Shamim <ahnafs@Ahnafs-MacBook-Air.local>
2022-01-01 21:43:28 -0500

    Testing icon in singleWeather

--- src/SingleWeather.js
+++ src/SingleWeather.js
@@ -1,18 +1,37 @@
 import { React, useState, useEffect } from "react";
 import getIcon from "./utils/getIcon";
+import Loading from "./Loading";
 
 const SingleWeather = ({ dt, temp, pressure, weather, city }) => {
   let [date, setDate] = useState(new Date());
   let [icon, setIcon] = useState("");
+  const [loading, setLoading] = useState(true);
   weather = weather[0];
   const { main, description, icon: preIcon } = weather;
 
+  const getIcon = async (icon) => {
+    try {
+      const response = await fetch(
+        `http://openweathermap.org/img/wn/${icon}.png`
+      );
+      const newIconBlob = await response.blob();
+      const newIconUrl = await URL.createObjectURL(newIconBlob);
+      setIcon(newIconUrl);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
-    getIcon(preIcon).then((newIcon) => setIcon(newIcon));
-    console.log("SingleWeather useEffect");
+    getIcon(preIcon);
+    setLoading(false);
+    console.log(icon);
   }, []);
 
-  console.log("Im here");
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <section className="h-full rounded p-6 bg-white flex items-center justify-around flex-col text-xl shadow-lg p-5 text-center capitalize pt-px lg:w-9/12 hover:border-2">
       <div className="border-b-4 border-indigo-500">
